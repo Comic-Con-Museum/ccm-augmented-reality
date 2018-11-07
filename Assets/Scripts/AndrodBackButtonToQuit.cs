30ed9883e2c215cd24d24536b8a2e17af043eabf
Nalin Ranjan <nalin.ranjan@outlook.com>
2018-11-07 14:21:16 -0800

    Added test 3D asset and Image Target; Updated android build settings

---
+++ Assets/Scripts/AndrodBackButtonToQuit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndrodBackButtonToQuit : MonoBehaviour {
+
+    void FixedUpdate() {
+        if (Application.platform == RuntimePlatform.Android) {
+            if (Input.GetKey(KeyCode.Escape)) {
+                Application.Quit();
+            }
+        }
+    }
+    
+}
