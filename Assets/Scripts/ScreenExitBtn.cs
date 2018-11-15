3a77e7b59ffb2ea696ced37d2eee2736b3fd9168
Yao Wang <yao_wang@intuit.com>
2018-11-15 03:22:54 -0800

    on screen exit button
    The button can be seen in Unity, but after build, it wont show in Android.

---
+++ Assets/Scripts/ScreenExitBtn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ScreenExitBtn : MonoBehaviour {
+
+    public void ExitBtn() {
+        Application.Quit();
+        Debug.Log("exit btn pressed");
+    }
+}
