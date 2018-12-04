4a1d359153072b04b54b3a5bb11e8a3dbbd544cb
Nalin Ranjan <nalin.ranjan@outlook.com>
2018-12-04 14:24:51 -0800

    Added TrackableEvent handler implementation for inactive items

---
+++ Assets/Scripts/InactiveExperienceTrackableEventHandler.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// A custom TrackableEvent handler for items that are not part of the current
+/// experience.
+/// 
+/// Rotates the augmentation object.
+/// </summary>
+public class InactiveExperienceTrackableEventHandler : BaseExperienceTrackableEventHandler {
+
+    public float rotationSpeed = 50.0f;
+    protected bool m_IsRotating = false;
+
+    void Update() {
+        if (m_IsRotating) {
+            foreach (Transform tf in transform) {
+				if (tf.CompareTag("ARModel")) {
+                    tf.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+                }
+            }
+        }
+    }
+
+    protected override void OnTrackingFoundImpl() {
+        m_IsRotating = true;
+    }
+
+    protected override void OnTrackingLostImpl() {
+        m_IsRotating = false;
+    }
+
+    protected override void OnTap() {
+        Debug.Log(gameObject.name + " tapped.");
+    }
+}
