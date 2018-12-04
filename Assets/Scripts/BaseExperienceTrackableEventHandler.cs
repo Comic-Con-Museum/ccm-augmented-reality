60d8b3b3adc137a57ec94538f450ef0d4c9250fe
Nalin Ranjan <nalin.ranjan@outlook.com>
2018-12-04 14:23:57 -0800

    Added base class for Experience TrackableEvent handlers

---
+++ Assets/Scripts/BaseExperienceTrackableEventHandler.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// Base class for TrackableEvent handlers used by augmentation objects in any
+/// experience.
+/// 
+/// Extends Vuforia's DefaultTrackableEventHandler.
+/// Inherit from this class to implement handlers for new experiences.
+/// </summary>
+public abstract class BaseExperienceTrackableEventHandler : DefaultTrackableEventHandler {
+
+    protected override void OnTrackingFound() {
+        base.OnTrackingFound();
+        OnTrackingFoundImpl();
+    }
+
+    protected override void OnTrackingLost() {
+        base.OnTrackingLost();
+        OnTrackingLostImpl();
+    }
+
+    protected virtual void OnTap() {}
+    protected virtual void OnSwipe() {}
+    protected virtual void OnTrackingFoundImpl() {}
+    protected virtual void OnTrackingLostImpl() {}
+}
